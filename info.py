c8ba239cb4876004123d31e926c01c1e60c1151c
মই এটি যাজাবৰ <ronichetia85@gmail.com>
2026-07-03 17:38:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,7 +109,7 @@ PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
 else:
-    ON_HEROKU = False
+    ON_HEROKU = True
 URL = environ.get("FQDN", "cool-billye-yok-5cdb3f81.koyeb.app")
 
 # Commands
