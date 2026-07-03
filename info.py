2540c585659e95e82222c15014c5033bc894f248
মই এটি যাজাবৰ <ronichetia85@gmail.com>
2026-07-03 17:42:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -110,7 +110,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = True
-URL = environ.get("FQDN", "cool-billye-yok-5cdb3f81.koyeb.app")
+URL = environ.get("FQDN", "cool-billye-yok-5cdb3f81.koyeb.app/")
 
 # Commands
 admin_cmds = [
