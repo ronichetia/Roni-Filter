4fa8dbe4553d41f2a30977a6a7f3b7e17fbceb40
মই এটি যাজাবৰ <ronichetia85@gmail.com>
2026-09-14 15:13:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ START_IMG = (environ.get('START_IMG', 'https://ibb.co/YTVvxV9w')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://ibb.co/TxV75CFJ')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://ibb.co/TDX9zHCF'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 
 
