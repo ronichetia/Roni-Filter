b3d720de4950d66835dbd4812c7d0f941d7d909a
মই এটি যাজাবৰ <ronichetia85@gmail.com>
2026-09-14 14:10:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,10 +69,10 @@ TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
 
 # Language & Quality & Season & Year
-LANGUAGES = ["original"]
-QUALITIES = ["original"]
-YEARS = [f'{i}' for i in range(all)]
-SEASONS = [f'season {i}'for i in range (all)]
+LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi", "marathi"]
+QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
+YEARS = [f'{i}' for i in range(2025, 2002,-1 )]
+SEASONS = [f'season {i}'for i in range (1 , 23)]
 
 # Pictures And Reaction
 START_IMG = (environ.get('START_IMG', 'https://ibb.co/rKdJBC8x')).split()
